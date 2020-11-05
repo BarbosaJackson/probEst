7cdecdb04234f95d792ae26d7f0f78a48bac6016
Jackson Barbosa <jbs@ic.ufal.br>
2020-11-04 21:34:11 -0300

    primeira questão

--- script.r
+++ script.r
@@ -1,8 +1,15 @@
-data <- subset(read.csv("dados_pademia_alagoas.csv"), municipio_residencia == "Arapiraca")
+data <- subset(read.csv("dados_pandemia_alagoas.csv"), municipio_residencia == "Arapiraca")
+
 labels <- unique(data$situacao_atual)
+
 t <- table(data$situacao_atual)
+
+arr <- 1:length(t)
 piepercent <- round(100*t/sum(t), 1)
+
 par(mar=c(2,0,2,2))
 pie(t,labels = piepercent, main="situação atual (porcentagem dos casos avaliados)", col = rainbow(length(t)), radius = 0.75, cex=.6)
 legend("topright", labels, cex = 0.6, fill = rainbow(length(t)))
-barplot(t)
+
+barplot(t, main="situação atual (porcentagem dos casos avaliados)", col=rainbow(length(t)), names.arg="")
+legend("topleft", labels, cex=0.6, fill=(rainbow(length(t))))
